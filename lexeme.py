3472cf759eed57ff8c13f8c42166e7476db47727
Veniamin VINOGRADOV <vvinogra@e1r3p7.unit.ua>
2020-02-28 21:36:32 +0200

    Continiued work

--- lexeme.py
+++ lexeme.py
@@ -4,11 +4,11 @@ from enum import Enum
 
 class RuleNode:
     def __init__(self, left_side, conclusion_op, right_side):
-        self._left_side = left_side
-        self._conclusion_op = conclusion_op
-        self._right_side = right_side
+        self.left_side = left_side
+        self.conclusion_op = conclusion_op
+        self.right_side = right_side
 
-        self.full_rule = ' '.join(map(str, self._left_side + [self._conclusion_op] + self._right_side))
+        self.full_rule = ' '.join(map(str, self.left_side + [self.conclusion_op] + self.right_side))
 
     def __str__(self):
         return self.full_rule
@@ -22,32 +22,16 @@ class RuleNode:
     def __eq__(self, other):
         return self.full_rule == other.full_rule
 
+    # def _solve_rule(self, graph):
 
-class FactNode:
-    def __init__(self, name, value=None):
-        self.name = name
-        self.value = value
+    def _get_fact_tokens(self):
+        left_side_facts = []
 
-    def __str__(self):
-        return self.name
+        for token in self.left_side:
+            if token.type == LexemeTypes.FACT:
+                left_side_facts.append(token)
 
-    def __hash__(self):
-        return hash(self.name)
-
-    def __eq__(self, other):
-        return self.name == other.name
-
-
-class Lexeme:
-    def __init__(self, l_type, l_value):
-        self.type = l_type
-        self.value = l_value
-
-    def __str__(self):
-        return self.value
-
-    def __repr__(self):
-        return self.value
+        return left_side_facts
 
 
 class LexemeTypes(Enum):
@@ -60,6 +44,154 @@ class LexemeTypes(Enum):
     OP_XOR = "^"
     OP_IMPLIES = "=>"
     OP_BICONDITION = "<=>"
+
+
+class Lexeme:
+    _infix_operators_list = [
+        LexemeTypes.OP_XOR.value,
+        LexemeTypes.OP_OR.value,
+        LexemeTypes.OP_AND.value,
+    ]
+
+    _prefix_operators_list = [
+        LexemeTypes.OP_NOT.value,
+    ]
+
+    _conclusion_operators_list = [
+        LexemeTypes.OP_IMPLIES.value,
+        LexemeTypes.OP_BICONDITION.value
+    ]
+
+    _other_operators_list = [
+        LexemeTypes.LEFT_BRACKET.value,
+        LexemeTypes.RIGHT_BRACKET.value,
+    ]
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def operators_list():
+        return [
+            *Lexeme._infix_operators_list,
+            *Lexeme._prefix_operators_list,
+            *Lexeme._conclusion_operators_list,
+            *Lexeme._other_operators_list
+        ]
+
+    @staticmethod
+    def is_fact(value):
+        return value in LexemeTypes.FACT.value
+
+
+class Fact:
+    def __init__(self, name):
+        self.name = name
+        # self.value = value
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    # def __invert__(self):
+    #     return ~self.value
+    #
+    # def __and__(self, other):
+    #     if type(other) is bool:
+    #         return self.value & other
+    #     elif type(other) is Fact:
+    #         return self.value & other.value
+    #
+    # def __or__(self, other):
+    #     if type(other) is bool:
+    #         return self.value | other
+    #     elif type(other) is Fact:
+    #         return self.value | other.value
+    #
+    # def __xor__(self, other):
+    #     if type(other) is bool:
+    #         return self.value ^ other
+    #     elif type(other) is Fact:
+    #         return self.value ^ other.value
+
+
+# class Lexeme:
+#     def __init__(self, l_type, l_value):
+#         self.type = l_type
+#         self.value = l_value
+#
+#     def __str__(self):
+#         return self.value
+#
+#     def __repr__(self):
+#         return self.value
+
+class Operator:
+    # Order values in increasing priority
+    infix_operators_list = [
+        LexemeTypes.OP_XOR.value,
+        LexemeTypes.OP_OR.value,
+        LexemeTypes.OP_AND.value,
+    ]
+
+    prefix_operators_list = [
+        LexemeTypes.OP_NOT.value,
+    ]
+
+    conclusion_operators_list = [
+        LexemeTypes.OP_IMPLIES.value,
+        LexemeTypes.OP_BICONDITION.value
+    ]
+
+    other_operators_list = [
+        LexemeTypes.LEFT_BRACKET.value,
+        LexemeTypes.RIGHT_BRACKET.value,
+    ]
+
+    def __init__(self, op):
+        self.op = op
+
+    def __str__(self):
+        return self.op
+
+    def __repr__(self):
+        return self.op
+
+    def is_infix_operator(self):
+        return self.op in Operator.infix_operators_list
+
+    def is_prefix_operator(self):
+        return self.op in Operator.prefix_operators_list
+
+    def is_conclusion_operator(self):
+        return self.op in Operator.conclusion_operators_list
+
+    def eval(self, left, right=None):
+        if self.op == LexemeTypes.OP_XOR.value:
+            return left ^ right
+        elif self.op == LexemeTypes.OP_OR.value:
+            return left | right
+        elif self.op == LexemeTypes.OP_AND.value:
+            return left & right
+        elif self.op == LexemeTypes.OP_NOT.value:
+            return ~left
+
+    @staticmethod
+    def operators_list():
+        return [
+            *Operator.infix_operators_list,
+            *Operator.prefix_operators_list,
+            *Operator.conclusion_operators_list,
+            *Operator.other_operators_list
+        ]
 
 
 LEXEME_SYMBOLS = [
--- lexer.py
+++ lexer.py
@@ -11,13 +11,12 @@ class Lexer:
 
         self._parsed_initial_facts = False
         self._parsed_queries = False
-        self._true_initial_facts = []
+        self._initial_facts = []
         self._queries = []
 
         self._graph = nx.MultiDiGraph()
-        # self._graph.add_node(1)
-        # self._graph.add_edge(1, 2)
-        # print(self._graph.nodes)
+
+        self._initialize_nodes()
 
     def tokenize_file(self):
         with open(self._filename, "r") as file:
@@ -38,6 +37,9 @@ class Lexer:
                 else:
                     rule = self._tokenize_rule(line)
 
+                    # for i in rule:
+                    #     print(i, end=" ")
+                    # print("")
                     # partitioned_rule = self._partition_rule(rule)
 
                     parsed_rule = self._parse_rule(rule)
@@ -76,13 +78,19 @@ class Lexer:
                 # print(split_line)
                 pass
 
+        self._resolve_queries()
+
         # for i in self._graph.nodes:
-        #     print("i.name = {}; i._rule_queries = {}".format(i.name, i._rule_queries))
+        #     if type(i) is Fact:
+        #         print("i = {}; i1 = {}".format(self._graph.nodes[i], i))
+
+        # print(self._graph[Fact("A")])
 
         nx.draw(self._graph,
                 with_labels=True,
                 arrows=True,
-                pos=nx.circular_layout(self._graph))
+                pos=nx.circular_layout(self._graph),
+                node_color=self._colorized_nodes())
 
         # nx.drawing.nx_pydot.write_dot(self._graph, "test.png")
 
@@ -111,7 +119,32 @@ class Lexer:
         #                        node_size=100)
         plt.show()
         # print(self._queries)
-        # print(self._true_initial_facts)
+        # print(self._initial_facts)
+
+    def _initialize_nodes(self):
+        for fact_val in LexemeTypes.FACT.value:
+            self._graph.add_node(Fact(fact_val), value=False)
+
+    # def _safely_add_node(self, node, **attr):
+    #     if node not in self._graph.nodes:
+    #         self._graph.add_node(node, **attr)
+    #         return True
+    #
+    #     return False
+
+    def _colorized_nodes(self):
+        ret = []
+
+        for node in self._graph.nodes():
+            if type(node) is Fact:
+                if self._graph.nodes[node]["value"]:
+                    ret.append("red")
+                else:
+                    ret.append("blue")
+            else:
+                ret.append("yellow")
+
+        return ret
 
     def _check_for_initial_facts(self, facts):
         if self._parsed_initial_facts:
@@ -119,7 +152,10 @@ class Lexer:
 
         for fact in facts[1:]:
             if fact in LexemeTypes.FACT.value:
-                self._true_initial_facts.append(Lexeme(LexemeTypes.FACT, fact))
+                self._graph.nodes[Fact(fact)]["value"] = True
+                self._initial_facts.append(Fact(fact))
+                # self._safely_add_node(Fact(fact), value=True)
+                # self._initial_facts.append(Lexeme(LexemeTypes.FACT, fact))
             else:
                 raise LexerException("Invalid fact in initial facts")
 
@@ -134,7 +170,9 @@ class Lexer:
 
         for query in queries[1:]:
             if query in LexemeTypes.FACT.value:
-                self._queries.append(Lexeme(LexemeTypes.FACT, query))
+                # self._queries.append(Fact(query))
+                # self._safely_add_node(Fact(query))
+                self._queries.append(Fact(query))
             else:
                 raise LexerException("Invalid fact in queries")
 
@@ -155,15 +193,15 @@ class Lexer:
             elif line[i].isspace():
                 continue
             elif line[i] in LexemeTypes.FACT.value:
-                lexeme_list.append(Lexeme(LexemeTypes.FACT, line[i]))
+                lexeme_list.append(Fact(line[i]))
             else:
                 cut_line = line[i:]
                 operand_found = False
 
-                for l_type in LEXEME_SYMBOLS:
-                    if cut_line.startswith(l_type.value):
-                        lexeme_list.append(Lexeme(l_type, l_type.value))
-                        chars_to_skip = len(l_type.value) - 1
+                for op in Operator.operators_list():
+                    if cut_line.startswith(op):
+                        lexeme_list.append(Operator(op))
+                        chars_to_skip = len(op) - 1
                         operand_found = True
 
                 if not operand_found:
@@ -177,7 +215,7 @@ class Lexer:
         conclusion_op = None
 
         for token in rule:
-            if token.type in LEXEME_IMPLICATION_TYPES:
+            if type(token) is Operator and token.is_conclusion_operator():
                 if not conclusion_op:
                     conclusion_op = token
                 else:
@@ -205,97 +243,191 @@ class Lexer:
         expect_operand = True
 
         for token in token_list:
-            if token.type == LexemeTypes.FACT:
+            if type(token) is Fact:
                 if not expect_operand:
                     raise LexerException("Expecting operator")
 
                 expect_operand = False
 
                 output.append(token)
-            elif token.type in LEXEME_PREFIX_OPERANDS:
-                if not expect_operand:
-                    raise LexerException("Expecting operand")
+            elif type(token) is Operator:
+                if token.is_prefix_operator():
+                    if not expect_operand:
+                        raise LexerException("Expecting operand")
 
-                expect_operand = True
+                    expect_operand = True
 
-                stack.append(token)
-            elif token.type == LexemeTypes.LEFT_BRACKET:
-                if not expect_operand:
-                    raise LexerException("Expecting operand")
+                    stack.append(token)
+                elif token.op == LexemeTypes.LEFT_BRACKET:
+                    if not expect_operand:
+                        raise LexerException("Expecting operand")
 
-                expect_operand = True
+                    expect_operand = True
 
-                stack.append(token)
-            elif token.type == LexemeTypes.RIGHT_BRACKET:
-                if expect_operand:
-                    raise LexerException("Expecting operand")
+                    stack.append(token)
+                elif token.op == LexemeTypes.RIGHT_BRACKET:
+                    if expect_operand:
+                        raise LexerException("Expecting operand")
 
-                expect_operand = False
+                    expect_operand = False
 
-                while stack:
-                    last_token = stack.pop()
+                    while stack:
+                        last_token = stack.pop()
 
-                    if last_token.type == LexemeTypes.LEFT_BRACKET:
-                        break
+                        if last_token.op == LexemeTypes.LEFT_BRACKET:
+                            break
 
-                    output.append(last_token)
+                        output.append(last_token)
 
-                if not stack:
-                    raise LexerException("Invalid brackets count")
+                    if not stack:
+                        raise LexerException("Invalid brackets count")
 
-            elif token.type in LEXEME_INFIX_OPERANDS:
-                if expect_operand:
-                    raise LexerException("Expecting operand")
+                elif token.is_infix_operator():
+                    if expect_operand:
+                        raise LexerException("Expecting operand")
 
-                expect_operand = True
+                    expect_operand = True
 
-                while stack:
-                    last_stack_token = stack[-1]
+                    while stack:
+                        last_stack_token = stack[-1]
 
-                    if last_stack_token.type in LEXEME_PREFIX_OPERANDS or \
-                            LEXEME_INFIX_OPERANDS.index(last_stack_token.type) >= \
-                            LEXEME_INFIX_OPERANDS.index(token.type):
-                        output.append(stack.pop())
-                    else:
-                        break
+                        if last_stack_token.is_prefix_operator() or \
+                                Operator.infix_operators_list.index(last_stack_token.op) >= \
+                                Operator.infix_operators_list.index(token.op):
+                            output.append(stack.pop())
+                        else:
+                            break
 
-                stack.append(token)
+                    stack.append(token)
 
         for op in stack:
-            if op.type in LEXEME_OPERANDS:
+            if op.is_prefix_operator() or \
+                    op.is_infix_operator():
                 output.append(op)
             else:
                 raise LexerException("Invalid brackets count")
 
         return output
 
+    def _solve_rpl(self, token_list):
+        stack = []
+
+        for token in token_list:
+            if type(token) is Fact:
+                stack.append(token)
+            elif type(token) is Operator:
+                if token.is_infix_operator():
+                    op1 = stack.pop()
+                    op2 = stack.pop()
+
+                    # if op1 in self._initial_facts:
+                    #     self._graph.nodes[op1]["value"] = True
+                    if self._graph.nodes[op1]["value"] is False:
+                        self._resolve_query(op1)
+
+                    if self._graph.nodes[op2]["value"] is False:
+                        self._resolve_query(op2)
+
+                    # if op2 in self._initial_facts:
+                    #     op2.value = True
+                    # elif op2.value is None:
+                    #     self._resolve_query(op2)
+
+                    stack.append(token.eval(self._graph.nodes[op1]["value"], self._graph.nodes[op2]["value"]))
+                elif token.is_prefix_operator():
+                    op = stack.pop()
+
+                    stack.append(token.eval(self._graph.nodes[op]["value"]))
+
+        return stack[0]
+
     def _get_fact_tokens(self, rule):
         left_side_facts = []
 
         for token in rule:
-            if token.type == LexemeTypes.FACT:
+            if type(token) is Fact:
                 left_side_facts.append(token)
 
         return left_side_facts
 
+    def _resolve_query(self, query):
+        result = []
+
+        if query in self._graph.nodes():
+            for neighbor in self._graph.neighbors(query):
+                rpl_left_side = self._convert_to_rpl(neighbor.left_side)
+
+                res = self._solve_rpl(rpl_left_side)
+
+                result.append(res)
+
+        if len(result):
+            self._graph.nodes[query]["value"] = all(x for x in result)
+        else:
+            self._graph.nodes[query]["value"] = False
+
+    def _resolve_queries(self):
+        for query in self._queries:
+            self._resolve_query(query)
+
+        # print(self._graph.nodes())
+        # for i in self._queries:
+        #     print("i = {}".format(i.value))
+
+    def _link_rule_node(self, rule_node):
+        if len(rule_node.right_side) == 1:
+            val = rule_node.right_side[0]
+
+            if type(val) is Fact:
+                self._graph.add_edge(val, rule_node)
+
+                lhs_fact_tokens = self._get_fact_tokens(rule_node.left_side)
+
+                for token in lhs_fact_tokens:
+                    self._graph.add_edge(rule_node,
+                                         token)
+            else:
+                raise LexerException("Invalid right rule side")
+
+        pass
+
     def _parse_right_side(self, right_side, parsed_left_side, rule_node):
         if len(right_side) == 1:
-            if right_side[0].type == LexemeTypes.FACT:
+            if type(right_side[0]) is Fact:
+                self._graph.add_edge(right_side[0], rule_node)
+
                 lhs_fact_tokens = self._get_fact_tokens(parsed_left_side)
 
                 for token in lhs_fact_tokens:
-                    self._graph.add_edge(FactNode(right_side[0].value),
-                                         FactNode(token.value), rule=rule_node)
-
+                    self._graph.add_edge(rule_node,
+                                         token)
+            else:
+                raise LexerException("Invalid right rule side")
         elif len(right_side) == 2:
             op, val = right_side
 
-            if op.type == LexemeTypes.OP_NOT:
+            if type(op) is Operator and op.op == LexemeTypes.OP_NOT.value \
+                    and type(val) is Fact:
                 lhs_fact_tokens = self._get_fact_tokens(parsed_left_side)
 
-                # for token in lhs_fact_tokens:
-                #     self._graph.add_edge(FactNode(val.value),
-                #                          FactNode(token.value), rule=rule_node)
+                self._graph.add_edge(val, rule_node)
+
+                for token in lhs_fact_tokens:
+                    self._graph.add_edge(rule_node,
+                                         token)
+            else:
+                raise LexerException("Invalid right rule side")
+        elif len(right_side) == 3:
+            operand1, operator, operand2 = right_side
+
+            # if operator.type == LexemeTypes.OP_AND:
+            #     lhs_fact_tokens = self._get_fact_tokens(parsed_left_side)
+            #
+            #     # for token in lhs_fact_tokens:
+            #     #     self._graph.add_edge(Fact(val.value),
+            #     #                          Fact(token.value), rule=rule_node)
+            # else:
+            #     raise LexerException("Invalid right rule side")
 
     def _parse_rule(self, rule):
 
@@ -307,7 +439,7 @@ class Lexer:
 
         self._parse_right_side(right_side, rpl_left_side, rule_node)
 
-        # for i in rpl_left_side:
-        #     print(i.value, end=" ")
+        # for i in solved_rpl:
+        #     print(i, end=" ")
         #
         # print("")
